b40b9b9df70ac99c3ce1637ea239978e3af84da6
Jhnvida <joao.vida.andre@gmail.com>
2026-08-06 15:02:58 -0300

    style: adiciona responsividade ao header e barra de filtros

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,16 @@ import styles from "./styles.module.css";
 
 export default function Header() {
     return (
-        <nav className={styles.nav}>
-            <h1 className={styles.logo}>Bookshelf.</h1>
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <h1 className={styles.logo}>Bookshelf.</h1>
 
-            <div className={styles.links}>
-                <a className={`${styles.link} ${styles.link_selected}`}>Biblioteca</a>
-                <a className={styles.link}>Quero Ler</a>
-                <a className={styles.link}>Favoritos</a>
-            </div>
-        </nav>
+                <div className={styles.links}>
+                    <a className={`${styles.link} ${styles.link_selected}`}>Biblioteca</a>
+                    <a className={styles.link}>Quero Ler</a>
+                    <a className={styles.link}>Favoritos</a>
+                </div>
+            </nav>
+        </header>
     );
 }
